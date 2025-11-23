a22a6668fb5b4ac3d7cc12013147a6f2eed5a362
Maxim Yurev <yurev.3411@gmail.com>
2025-11-23 08:54:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Serverless! 🚀\n", 200, {'Content-Type': 'text/plain'}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
